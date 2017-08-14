28f14b3c129b1e20404bfafa2c49c921c5f73e97
Edward Banner <edward.banner@gmail.com>
2017-08-14 16:02:57 -0400

    Committing old changes

--- experiments/jobs/test.py
+++ experiments/jobs/test.py
@@ -11,8 +11,13 @@ from condor_create import make_exps
 exp_group = os.path.basename(__file__).split('.')[-2]
 
 
-args = {'-nb-filter': [1, 2, 3, 4, 5],
-        '-hidden-dim': [32, 64]
+args = {'-callbacks': ['cb,ss,fl,es,cv'],
+        '-nb-epoch': [32],
+        '-trainer': ['SharedCNNSiameseTrainer'],
+        '-nb-train': [1.],
+        '-target': ['abstracts'],
+        '-reg': [0., 1e-6],
+        '-dropout-prob': [0., .5],
 }
 
 make_exps(exp_group, args, grid_search=True)
--- experiments/quick_run.sh
+++ experiments/quick_run.sh
@@ -1,19 +1,21 @@
-#!/usr/local/bin/bash
+#!/bin/bash
 
-cuda=/opt/cuda-7.5
+# Theano setup
+module load gnu-4.4-compilers
+module load fftw-3.3.3
+module load perl-5.20.0
+module load platform-mpi
+module load slurm-14.11.8
 
-cuDNNv4=/u/ebanner/builds/cudnn-7.0-linux-x64-v4.0-prod
-cuDNNv5=/u/ebanner/builds/cudnn-7.5-linux-x64-v5.0-ga
+CUDAVER="7.5"
+module load cuda-${CUDAVER}
 
-cuDNN=$cuDNNv5
+export PATH=/shared/apps/cuda${CUDAVER}/bin:$PATH
+export LD_LIBRARY_PATH=/shared/apps/cuda${CUDAVER}/lib64:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/home/ekstrand-abueg.m/.local/lib/:$LD_LIBRARY_PATH
+export CUDA_HOME=/shared/apps/cuda${CUDAVER}/
 
-export LD_LIBRARY_PATH=$cuDNN/lib64:$cuda/lib64:$LD_LIBRARY_PATH
-export CPATH=$cuDNN/include:$CPATH
-export LIBRARY_PATH=$cuDNN/lib64:$LD_LIBRARY_PATH
-export CUDNN_PATH=$cuDNN
-
-export CUDA_HOME=$cuda
+export PYTHONPATH="/home/banner.ed/pico-vectors/preprocess:$PYTHONPATH"
 
 cd code
-
-THEANO_FLAGS=device=cpu,optimizer=fast_compile python train.py $@
+THEANO_FLAGS=device=cpu,optimizer=fast_compile,mode=FAST_RUN,floatX=float32 python train.py $@
